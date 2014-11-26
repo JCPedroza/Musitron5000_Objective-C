8d1f312ec997d8f5611dcbc965ba2ca20ad5d5dc
JCPedroza <nemesis4go10@hotmail.com>
2014-11-26 16:16:24 -0600

    first commit

---
+++ UnitTest/UnitTest.h
@@ -0,0 +1,13 @@
+//
+//  UnitTest.h
+//  UnitTest
+//
+//  Created by JCPedroza on 26/11/14.
+//  Copyright (c) 2014 Smash. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTest : SenTestCase
+
+@end
---
+++ UnitTest/UnitTest.m
@@ -0,0 +1,32 @@
+//
+//  UnitTest.m
+//  UnitTest
+//
+//  Created by JCPedroza on 26/11/14.
+//  Copyright (c) 2014 Smash. All rights reserved.
+//
+
+#import "UnitTest.h"
+
+@implementation UnitTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UnitTest");
+}
+
+@end
